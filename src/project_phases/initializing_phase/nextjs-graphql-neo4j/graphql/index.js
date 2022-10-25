b1d4d46b548582cbfa90a82d79ff38ba60f35108
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-25 22:09:05 +0300

    WM-58 getting start with graphql

--- src/project_phases/initializing_phase/nextjs-graphql-neo4j/graphql/index.js
+++ src/project_phases/initializing_phase/nextjs-graphql-neo4j/graphql/index.js
@@ -1,9 +1,24 @@
 import { ApolloServer } from "apollo-server-micro";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 import schema from "./schema";
+import { typeDefs } from "./typeDefs";
+require("dotenv").config();
+
+const driver = neo4j.driver(
+  process.env.NEO4J_URI,
+  neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD),
+);
+
+const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
 
 export const server = new ApolloServer({
-  schema,
+  schema: await neoSchema.getSchema(),
   playground: true,
   plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
 });
+
+// new ApolloServer({
+//   schema,
+//   playground: true,
+//   plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
+// });
--- src/project_phases/initializing_phase/nextjs-graphql-neo4j/pages/api/graphql.js
+++ src/project_phases/initializing_phase/nextjs-graphql-neo4j/pages/api/graphql.js
@@ -3,6 +3,20 @@ import { server } from "../../graphql";
 const startServer = server.start();
 
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://studio.apollographql.com",
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept",
+  );
+  if (req.method === "OPTIONS") {
+    res.end();
+    return false;
+  }
+
   await startServer;
   await server.createHandler({ path: "/api/graphql" })(req, res);
 }
