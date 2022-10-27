9c71e0ea816b10f72a95a1bdf03e20c46bcbbb5f
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-27 12:12:10 +0300

    WM-37 finalizing the mini project

--- src/project_phases/initializing_phase/google-authentication-firebase/firbase/index.js
+++ src/project_phases/initializing_phase/google-authentication-firebase/firbase/index.js
@@ -1,14 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-require("dotenv").config();
 
 const firebaseConfig = {
-  apiKey: process.env.GOOGLE_API_KEY,
-  authDomain: process.env.GOOGLE_AUTH_DOMAIN,
-  projectId: process.env.GOOGLE_PROJECT_ID,
-  storageBucket: process.env.GOOGLE_STORAGE_BUCKET,
-  messagingSenderId: process.env.GOOGLE_MESSAGEING_SENDER,
-  appId: process.env.GOOGLE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_GOOGLE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_GOOGLE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_GOOGLE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_GOOGLE_MESSAGEING_SENDER,
+  appId: process.env.NEXT_PUBLIC_GOOGLE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -16,10 +15,18 @@ export const auth = getAuth(app);
 
 const provider = new GoogleAuthProvider();
 
-export const signInWithGoogle = () => {
+export const signInWithGoogle = (setName, setEmail, setProfilePic) => {
   signInWithPopup(auth, provider)
     .then((result) => {
-      console.log(result);
+      const name = result.user.displayName;
+      const email = result.user.email;
+      const profilePic = result.user.photoURL;
+      localStorage.setItem("name", name);
+      localStorage.setItem("email", email);
+      localStorage.setItem("profilePic", profilePic);
+      setName(name);
+      setEmail(email);
+      setProfilePic(profilePic);
     })
     .catch((error) => {
       console.log(error);
