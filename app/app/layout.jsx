e0b5f9647947f1c70e3d2677d141026edba5f75c
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-31 02:38:50 +0200

    WM-40 editing layout frontend

---
+++ app/app/layout.jsx
@@ -0,0 +1,31 @@
+import NavBar from "@/components/NavBar";
+import TopBar from "@/components/TopBar";
+import { styles } from "@/utils/styles";
+
+export default function Layout({
+  children,
+  withImg,
+  withNav,
+  active,
+  setActive,
+}) {
+  return (
+    <div className="app">
+      <TopBar withImg={withImg} setActive={setActive} />
+      <div className="app-body">{children}</div>
+      <div>{withNav && <NavBar active={active} setActive={setActive} />}</div>
+      <style jsx>{`
+        .app {
+          ${styles.flexColumn};
+          height: 100vh;
+          overflow: hidden;
+        }
+        .app-body {
+          height: calc(100vh - 10rem);
+          overflow: auto;
+          padding: 0.6rem;
+        }
+      `}</style>
+    </div>
+  );
+}
