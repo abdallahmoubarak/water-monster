dfcb350f67688a5b5e70377aca321215ce0537b6
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-31 22:12:54 +0200

    WM-64 adding pwa to the app

--- app/next.config.js
+++ app/next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa");
+
+const pwa = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+});
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = { nextConfig, pwa };
