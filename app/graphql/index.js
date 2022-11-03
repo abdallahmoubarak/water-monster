4b2e475b1125f05d6d422870e6451059bbb17909
Abdallah <abdallah.moubarak02@gmail.com>
2022-11-03 18:30:03 +0200

    WM-79 adding ogm for authentications

--- app/graphql/index.js
+++ app/graphql/index.js
@@ -1,11 +1,14 @@
 import { ApolloServer } from "apollo-server-micro";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 import { typeDefs } from "./typeDefs";
+import { resolvers } from "./resolvers";
 import { Neo4jGraphQL } from "@neo4j/graphql";
+import { Neo4jGraphQLAuthJWTPlugin } from "@neo4j/graphql-plugin-auth";
 import neo4j from "neo4j-driver";
+import { OGM } from "@neo4j/graphql-ogm";
 import "ts-tiny-invariant";
 
-const driver = neo4j.driver(
+export const driver = neo4j.driver(
   process.env.NEXT_PUBLIC_NEO4J_URI,
   neo4j.auth.basic(
     process.env.NEXT_PUBLIC_NEO4J_USER,
@@ -13,10 +16,22 @@ const driver = neo4j.driver(
   ),
 );
 
-const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
+const ogm = new OGM({ typeDefs, driver });
+ogm.init();
+export const User = ogm.model("User");
+
+const neoSchema = new Neo4jGraphQL({ typeDefs, resolvers, driver });
 
 export const server = new ApolloServer({
   schema: await neoSchema.getSchema(),
+  context: ({ req }) => ({ req }),
   playground: true,
-  plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
+  plugins: [
+    ApolloServerPluginLandingPageGraphQLPlayground,
+    {
+      auth: new Neo4jGraphQLAuthJWTPlugin({
+        secret: process.env.NEXT_PUBLIC_JWT_SECRET,
+      }),
+    },
+  ],
 });
