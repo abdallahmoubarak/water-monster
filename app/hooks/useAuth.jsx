c04c6a864631fcc765068a6c83678b71cedcf3b7
Abdallah <abdallah.moubarak02@gmail.com>
2022-11-05 15:21:56 +0200

    WM-65 creating sign hook frontend

--- app/hooks/useAuth.jsx
+++ app/hooks/useAuth.jsx
@@ -46,6 +46,45 @@ export const useSignUp = (setMsg) => {
   });
 };
 
+/*               signIn hook             */
+
+const signIn = async ({ email, password }) => {
+  const signInMutation = gql`
+    mutation ($email: String!, $password: String!) {
+      signIn(email: $email, password: $password) {
+        token
+        user {
+          id
+          name
+          email
+          profile_url
+        }
+      }
+    }
+  `;
+  const variables = {
+    email,
+    password,
+  };
+  const res = await graphQLClient.request(signInMutation, variables);
+  return res.signIn;
+};
+export const useSignIn = (setMsg) => {
+  return useMutation(signIn, {
+    onSuccess: (res) => {
+      localStorage.setItem("JWT", res?.token);
+      localStorage.setItem("User", res?.user);
+      client.setQueryData(["JWT"], res?.token);
+      client.setQueryData(["User"], res?.user);
+    },
+    onError: (err) => {
+      setMsg(err.message);
+    },
+  });
+};
+
+/*         get user query function and hook        */
+
 const getUser = async () => {
   const me = gql`
     query {
