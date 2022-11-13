83d27c67b9460d7807d5085753a628a2bc423e2c
Abdallah <abdallah.moubarak02@gmail.com>
2022-11-13 23:26:33 +0200

    WM-65 updating request card frontend

--- app/components/admin/RequestCard.jsx
+++ app/components/admin/RequestCard.jsx
@@ -7,20 +7,37 @@ export default function ReuestCard({ request }) {
   return (
     <>
       <div className="request-card">
-        <div className="request-icon">
-          <Image src={installation} alt={""} width={80} />
+        <div className="request-header">
+          <div>
+            {request?.title} <span>request</span>
+          </div>
+          <div className="request-time">
+            {dateTimeChanger(request?.createdAt)}
+          </div>
         </div>
-        <div>User Name</div>
-        <div>
-          <span>Type: </span>
-          {request.title}
-        </div>
-        <div>
-          <span>State: </span>
-          {request.state}
-        </div>
+        <div className="request-body">
+          <div className="request-icon">
+            <Image src={installation} alt={""} width={80} />
+          </div>
+          <div className="request-info">
+            <div className="request-creator">{request?.creator?.name}</div>
 
-        <div className="request-time">{dateTimeChanger(request.createdAt)}</div>
+            <div>
+              <span>Waiting: </span>
+              {request?.state}
+            </div>
+            <div>
+              <span>Prefered date: </span>
+              {request?.date}
+            </div>
+          </div>
+        </div>
+        <div className="request-footer">
+          <div>requested</div>
+          <div>approved</div>
+          <div>installed</div>
+          <div>done</div>
+        </div>
       </div>
       <style jsx>{`
         .request-card {
@@ -31,14 +48,38 @@ export default function ReuestCard({ request }) {
           ${styles.boxshadow};
           ${styles.borderRadiusp3rem};
           width: 20rem;
-          padding: 1rem;
-          gap: 0.6rem;
+          padding: 0.6rem 1rem;
+          ${styles.userSelect};
+          cursor: pointer;
+          gap: 1rem;
+        }
+        .request-header {
+          ${styles.fontSize1p2rem}
+        }
+        .request-body {
+          ${styles.flexAligncenter}
+        }
+        .request-info {
+          display: -webkit-box;
+          display: -ms-flexbox;
+          display: flex;
+          ${styles.flexColumn};
+          gap: 0.3rem;
+        }
+        .request-icon {
+          ${styles.flexBothcenter};
+        }
+        .request-creator {
+          ${styles.fontSize1p2rem}
         }
         .request-time {
           ${styles.fontSizep8rem};
         }
-        .request-icon {
-          ${styles.flexBothcenter};
+        .request-footer {
+          ${styles.flexAligncenter};
+          ${styles.justifyBetween};
+          ${styles.fontSizep8rem};
+          gap: 0.2rem;
         }
       `}</style>
     </>
--- app/hooks/gql/request.js
+++ app/hooks/gql/request.js
@@ -6,40 +6,9 @@ export const requestsQuery = gql`
       title
       createdAt
       state
-    }
-  }
-`;
-
-export const createInstallationRequestMutation = gql`
-  mutation (
-    $id: ID!
-    $name: String!
-    $size: String!
-    $date: Date!
-    $address: String!
-  ) {
-    createRequests(
-      input: [
-        {
-          title: "Installation"
-          state: "approval"
-          date: $date
-          creator: { connect: { where: { node: { id: $id } } } }
-          container: {
-            create: {
-              node: {
-                name: $name
-                size: $size
-                address: $address
-                user: { connect: { where: { node: { id: $id } } } }
-              }
-            }
-          }
-        }
-      ]
-    ) {
-      requests {
-        id
+      date
+      creator {
+        name
       }
     }
   }
